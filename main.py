9aeb29fae787b1360c773184295703554ce88dd6
LJaremek <Jaremek.Lukasz@gmail.com>
2024-03-09 18:19:28 +0100

    refactor code for automatization

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from itertools import product
 import json
 
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 from torch import optim
 import torch
 
 from models import SimpleCNN, AdvancedCNN
+from tools import run_model
 
 
 with open("data_mean_std.json", "r", -1, "utf-8") as f:
@@ -17,102 +17,64 @@ data_std = data_desc["std"]
 
 IMAGE_SIZE = 32
 
-transform = transforms.Compose([
-    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=data_mean,
-        std=data_std
-        ),
-])
+transform_list = (
+    transforms.Compose([
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=data_mean,
+            std=data_std
+            )
+    ]),
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
 print()
 
-dataset = datasets.ImageFolder(root="data/train", transform=transform)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-
-print(dataset.shape)
-
+dataset = datasets.ImageFolder(root="data/train")
 classes_number = len(dataset.classes)
-
-
-def plot_images(images, labels, dataset):
-    images = images.view(-1, 3, IMAGE_SIZE, IMAGE_SIZE)
-
-    plt.figure(figsize=(2, 2))
-    plt.axis("off")
-
-    # Zmiana kolejności wymiarów z (C, H, W) na (H, W, C)
-    img = images[0].permute(1, 2, 0)
-
-    label = labels[0].item()
-    plt.imshow(img)
-    plt.title(f"Label: {dataset.classes[label]}")
-    plt.show()
-
 
 available_models = (
     SimpleCNN,
     AdvancedCNN
 )
 
-EPOCHS = 10
+learning_rates = (
+    0.00001,
+)
 
+batch_sizes = (
+    64,
+)
 
-for model in available_models:
+EPOCHS = 20
+
+products = product(
+    available_models,
+    learning_rates,
+    batch_sizes,
+    transform_list
+)
+
+for model, learning_rate, batch_size, transform in products:
     print("-----", model.name, "-----")
+    print("PARAMS:")
+    print(f"\tLearning rate: {learning_rate}")
+    print(f"\tBatch size: {batch_size}")
     model = model(num_classes=classes_number).to(device)
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    model.train()
-
-    for epoch in range(EPOCHS):
-        epoch_losses = []
-        for images, labels in dataloader:
-            images, labels = images.to(device), labels.to(device)
-
-            optimizer.zero_grad()
-
-            outputs = model(images)
-
-            # print("L:", [int(el) for el in labels])
-            # print("O:", [int(el.argmax(dim=0)) for el in outputs])
-            # print("O:", outputs)
-
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            epoch_losses.append(loss.item())
-
-        avg_loss = sum(epoch_losses)/len(epoch_losses)
-        print(f"Epoch [{epoch+1}/{EPOCHS}], Loss: {avg_loss:.4f}")
-
-    test_dataset = datasets.ImageFolder(root="data/test", transform=transform)
-    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-    model.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-
-    with torch.no_grad():
-        for images, labels in test_dataloader:
-            images, labels = images.to(device), labels.to(device)
-
-            outputs = model(images)
-
-            loss = criterion(outputs, labels)
-            test_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    avg_loss = test_loss / len(test_dataloader.dataset)
-    accuracy = 100 * correct / total
-
-    print(f"Test Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%")
+    run_model(
+        device,
+        model,
+        transform,
+        "data/train",
+        "data/test",
+        batch_size,
+        EPOCHS,
+        criterion,
+        optimizer
+    )
--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from torch import optim
+import torch.nn as nn
 import numpy as np
+import torch
 
 
 def calc_data_mean_std(
@@ -37,3 +41,133 @@ def calc_data_mean_std(
         "mean": mean.tolist(),
         "std": std.tolist()
     }
+
+
+def plot_images(images, labels, dataset, image_size: tuple[int, int]) -> None:
+    images = images.view(-1, 3, image_size[0], image_size[1])
+
+    plt.figure(figsize=(2, 2))
+    plt.axis("off")
+
+    # Zmiana kolejności wymiarów z (C, H, W) na (H, W, C)
+    img = images[0].permute(1, 2, 0)
+
+    label = labels[0].item()
+    plt.imshow(img)
+    plt.title(f"Label: {dataset.classes[label]}")
+    plt.show()
+
+
+def train_model(
+        device: torch.device,
+        model: nn.Module,
+        transform: transforms.Compose,
+        data_path: str,
+        batch_size: int,
+        epochs: int,
+        criterion,
+        optimizer: optim.Optimizer,
+        printing: bool = True
+        ) -> None:
+
+    dataset = datasets.ImageFolder(root=data_path, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    model.train()
+
+    for epoch in range(epochs):
+        epoch_losses = []
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            # print("L:", [int(el) for el in labels])
+            # print("O:", [int(el.argmax(dim=0)) for el in outputs])
+            # print("O:", outputs)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            epoch_losses.append(loss.item())
+
+        avg_loss = sum(epoch_losses)/len(epoch_losses)
+        if printing:
+            print(f"Epoch [{epoch+1}/{epochs}], Loss: {avg_loss:.4f}")
+
+
+def valid_model(
+        device: torch.device,
+        model: nn.Module,
+        transform: transforms.Compose,
+        data_path: str,
+        batch_size: int,
+        criterion,
+        printing: bool = True
+        ) -> None:
+
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    dataset = datasets.ImageFolder(data_path, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    model.eval()
+
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    avg_loss = test_loss / len(dataloader.dataset)
+    accuracy = 100 * correct / total
+
+    if printing:
+        print(f"Test Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+
+def run_model(
+        device: torch.device,
+        model: nn.Module,
+        transform: transforms.Compose,
+        train_path: str,
+        test_path: str,
+        batch_size: int,
+        epochs: int,
+        criterion,
+        optimizer: optim.Optimizer,
+        printing: bool = True
+        ) -> None:
+
+    train_model(
+        device,
+        model,
+        transform,
+        train_path,
+        batch_size,
+        epochs,
+        criterion,
+        optimizer,
+        printing
+        )
+
+    valid_model(
+        device,
+        model,
+        transform,
+        test_path,
+        batch_size,
+        criterion,
+        printing
+    )
